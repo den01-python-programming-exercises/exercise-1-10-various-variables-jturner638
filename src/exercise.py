508c44e8bbcb4c1dd90fd338916c8e44e824bb51
Justin Turner <mrfathed@protonmail.com>
2021-12-03 14:16:01 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,7 +1,7 @@
 def main():
     #write your code below this line
-    chicken_quantity = 3
-    bacon_quantity = 5.5
+    chicken_quantity = 9000
+    bacon_quantity = 0.1
     print("Chicken:")
     print(chicken_quantity)
     print("Bacon (kg):")
